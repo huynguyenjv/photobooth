f7faa69975ef3fa0df23e402395012b38ce65525
Huy Nguyen <nguyenmaibaohuy2003@gmail.com>
2026-02-08 23:22:00 +0700

    feature: build for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Photobooth/',  // GitHub Pages repo name
+  build: {
+    outDir: 'docs',  // Output to docs folder for GitHub Pages
+  },
 })
